8c1bc39594c5083866fa5962e6e796f08fecdc61
Prince <juniortlou212@gmail.com>
2026-09-23 10:58:51 +0200

    filled in user model for db

--- backend/appointment_backend/models.py
+++ backend/appointment_backend/models.py
@@ -1,10 +1,33 @@
 from django.db import models
-
+from django.contrib.auth.hashers import make_password, check_password
 # Create your models here.
 class User(models.Model):
-    pass
+    def set_password(self, password):
+        self.password_hash = make_password(password)
 
-
+    def check_password(self, password):
+        return check_password(password, self.password_hash)
+    # Django expeccts two choices for some reason
+    #(Value, Label)
+    ROLES = (
+        ("PATIENT", "Patient"),
+        ("DOCTOR","Doctor"),
+        ("ADMIN", "Admin"),
+    )
+    ACCOUNT_STATUSES = (
+        ("ACTIVE", "Active"),
+        ("INACTIVE", "Inactive"),
+        ("SUSPENDED", "Suspended"),
+    )
+    user_id = models.BigAutoField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100, unique=True)
+    password_hash = models.CharField(max_length=128)
+    role = models.CharField(max_length=10, choices=ROLES)
+    account_status = models.CharField(max_length=20, choices=ACCOUNT_STATUSES, default="ACTIVE")
+    
+    
 class Patient(models.Models):
     pass
 
