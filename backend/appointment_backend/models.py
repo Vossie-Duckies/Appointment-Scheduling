26d4108a9ce307748cd0986da2192dc46b63424b
Prince <juniortlou212@gmail.com>
2026-09-23 10:14:16 +0200

    added structure for database models

--- backend/appointment_backend/models.py
+++ backend/appointment_backend/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    pass
+
+
+class Patient(models.Models):
+    pass
+
+class Doctor(models.Models):
+    pass
+
+class Administrator(models.Models):
+    pass
+
+class Doctor_slot(models.Models):
+    pass
+
+class Appointment(models.Models):
+    pass
+
+class Notification(models.Models):
+    pass
