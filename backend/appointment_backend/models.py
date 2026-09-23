6811f1a2b04bd19d23fa169b89c08327021993fd
Prince <juniortlou212@gmail.com>
2026-09-23 12:31:03 +0200

    migrated db

--- backend/appointment_backend/models.py
+++ backend/appointment_backend/models.py
@@ -90,7 +90,7 @@ class Appointment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True, blank=True)
     status = models.CharField(max_length=20, choices=STATUS, default="BOOKED")
-    reason = models.TextField(null=True, blank=True)
+    reason = models.TextField()
 
     
 
