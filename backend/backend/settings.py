7be4fd3cd66ac3e8a7ca7eca23a96b8dac37a4f7
Prince <juniortlou212@gmail.com>
2026-09-25 23:12:57 +0200

    added email information for testing purposes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import os 
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,3 +128,13 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'juniortlou55@gmail.com'
+EMAIL_HOST_PASSWORD = 'trihptwzqtzhuzwz'
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
