73957532243a2f5f35292d5592d97b149ea9f39f
Prince <juniortlou212@gmail.com>
2026-09-26 14:15:26 +0200

    added user manager class using django

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.base_user import BaseUserManager
 
+from django.contrib.auth.hashers import make_password, check_password
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        user = self.create_user(
+            email=email,
+            password=password,
+            **extra_fields
+        )
+
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
